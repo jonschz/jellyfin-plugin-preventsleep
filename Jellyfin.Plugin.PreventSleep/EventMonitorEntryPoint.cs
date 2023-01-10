454ed95c4b4e448f650b08e19f79e39c3e2dc243
jonschz <jonschz@users.noreply.github.com>
2023-01-10 09:29:08 +0100

    implementing basic functionality

--- Jellyfin.Plugin.PreventSleep/EventMonitorEntryPoint.cs
+++ Jellyfin.Plugin.PreventSleep/EventMonitorEntryPoint.cs
@@ -15,6 +15,9 @@ along with this program. If not, see<http://www.gnu.org/licenses/>.
 */
 
 using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 // using MediaBrowser.Controller.Configuration;
 using MediaBrowser.Controller.Library;
@@ -25,6 +28,17 @@ using Microsoft.Extensions.Logging;
 
 namespace Jellyfin.Plugin.PreventSleep;
 
+// based on https://stackoverflow.com/a/60512156
+[Flags]
+public enum EXECUTION_STATE : uint
+{
+    ES_AWAYMODE_REQUIRED = 0x00000040,
+    ES_CONTINUOUS = 0x80000000,
+    ES_DISPLAY_REQUIRED = 0x00000002,
+    ES_SYSTEM_REQUIRED = 0x00000001
+    // ES_USER_PRESENT = 0x00000004
+}
+
 public class EventMonitorEntryPoint : IServerEntryPoint
 {
     private readonly ISessionManager _sessionManager;
@@ -33,6 +47,11 @@ public class EventMonitorEntryPoint : IServerEntryPoint
     private readonly ILogger<EventMonitorEntryPoint> _logger;
     // private readonly ILoggerFactory _loggerFactory;
     // private readonly IFileSystem _fileSystem;
+
+    private Timer? _busyTimer;
+    private bool _isBusy;
+
+    private bool _disposed;
 
     public EventMonitorEntryPoint(
         ISessionManager sessionManager,
@@ -45,11 +64,6 @@ public class EventMonitorEntryPoint : IServerEntryPoint
         _sessionManager = sessionManager;
         // _config = config;
         // _fileSystem = fileSystem;
-    }
-
-    public void Dispose()
-    {
-        GC.SuppressFinalize(this);
     }
 
     public Task RunAsync()
@@ -66,6 +80,12 @@ public class EventMonitorEntryPoint : IServerEntryPoint
     private void SessionManager_PlaybackProgress(object? sender, PlaybackProgressEventArgs e)
     {
         _logger.LogInformation("Plugin: Playback progress");
+        /*
+        Must check the timer here because PlaybackStart is not necessarily triggered for every stream.
+        Example: Server is rebooted while a stream is running and the stream reconnects.
+        Then the new instance of the server never triggers a PlaybackStart event.
+        */
+        StartBusyTimer();
     }
 
     private void SessionManager_PlaybackStop(object? sender, PlaybackStopEventArgs e)
@@ -76,5 +96,70 @@ public class EventMonitorEntryPoint : IServerEntryPoint
     private void SessionManager_PlaybackStart(object? sender, PlaybackProgressEventArgs e)
     {
         _logger.LogInformation("Plugin: Playback start");
+        StartBusyTimer();
+    }
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+    private static extern uint SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+    private void UpdateBusyState(object? state)
+    {
+        var newIsBusy = _sessionManager.Sessions.Any(i => i.NowPlayingItem is not null);
+
+        if (_isBusy != newIsBusy)
+        {
+            _logger.LogInformation("New busy state: {State}", newIsBusy);
+            _isBusy = newIsBusy;
+        }
+
+        if (_isBusy)
+        {
+            // Repeat regular calls to SetThreadExecutionState.
+            // We can't use ES_CONTINUOUS because the calls may be made from separate multiprocessing instances,
+            // so there is no guarantee that the flag will be cleared in the correct process.
+            uint oldState = SetThreadExecutionState(EXECUTION_STATE.ES_SYSTEM_REQUIRED);
+            if (oldState == 0)
+            {
+                _logger.LogError("Call to SetThreadExecutionState failed");
+            }
+        }
+        else
+        {
+            // TODO implement stopping the timer
+            // DisposeBusyTimer();
+        }
+    }
+
+    private void DisposeBusyTimer()
+    {
+        _busyTimer?.Dispose();
+        _busyTimer = null;
+    }
+
+    private void StartBusyTimer()
+    {
+        // The minimum time to sleep in Windows is 1 minute, so updating the busy state every 30 seconds is on the safe side
+        _busyTimer ??= new Timer(UpdateBusyState, null, TimeSpan.Zero, TimeSpan.FromSeconds(30));
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        if (disposing)
+        {
+            DisposeBusyTimer();
+        }
+
+        _disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
     }
 }
