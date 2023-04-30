8ce80894330abb2a9d1ea606bed051e2163b936e
jonschz <jonschz@users.noreply.github.com>
2023-04-30 22:03:56 +0200

    fix linter warnings in VanaraKernel32.cs

--- Jellyfin.Plugin.PreventSleep/VanaraKernel32.cs
+++ Jellyfin.Plugin.PreventSleep/VanaraKernel32.cs
@@ -1,33 +1,29 @@
 ﻿/*
-    MIT License
+    Copyright (c) 2017 David Hall 2023 Faheem Pervez, Jonathan Schulz
 
-    Copyright (c) 2017 David Hall
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
 
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see<http://www.gnu.org/licenses/>.
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+
+    Originally published under MIT License, re-published with modifications
+    under GPLv3.
+    https://github.com/dahall/Vanara/commit/153533f7e07bb78119dee90520ed5af6b5b584ae
 */
 
-// https://github.com/dahall/Vanara/commit/153533f7e07bb78119dee90520ed5af6b5b584ae
+#pragma warning disable CA1401 // PInvokesShouldNotBeVisible
 
 using System;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using System.Runtime.ConstrainedExecution;
 using System.Runtime.InteropServices;
 using Microsoft.Win32.SafeHandles;
 
@@ -43,29 +39,6 @@ public static class VanaraPInvokeKernel32
         PowerRequestExecutionRequired,
     }
 
-    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool PowerClearRequest(SafePowerRequestObject PowerRequest, POWER_REQUEST_TYPE RequestType);
-
-    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-    public static extern SafePowerRequestObject PowerCreateRequest([In] REASON_CONTEXT Context);
-
-    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool PowerSetRequest(SafePowerRequestObject PowerRequest, POWER_REQUEST_TYPE RequestType);
-
-    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-    [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool CloseHandle(IntPtr hObject);
-
-    public class SafePowerRequestObject : SafeKernelHandle
-    {
-        public SafePowerRequestObject(IntPtr preexistingHandle, bool ownsHandle = true) : base(preexistingHandle, ownsHandle) { }
-
-        private SafePowerRequestObject() : base() { }
-    }
-
     public enum DIAGNOSTIC_REASON : uint
     {
         DIAGNOSTIC_REASON_SIMPLE_STRING = 0x00000001,
@@ -78,41 +51,77 @@ public static class VanaraPInvokeKernel32
         DIAGNOSTIC_REASON_VERSION = 0
     }
 
+    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool PowerClearRequest(SafePowerRequestObject powerRequest, POWER_REQUEST_TYPE requestType);
+
+    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+    public static extern SafePowerRequestObject PowerCreateRequest([In] REASON_CONTEXT context);
+
+    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool PowerSetRequest(SafePowerRequestObject powerRequest, POWER_REQUEST_TYPE requestType);
+
+    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool CloseHandle(IntPtr hObject);
+
+    public class SafePowerRequestObject : SafeKernelHandle
+    {
+        public SafePowerRequestObject(IntPtr preexistingHandle, bool ownsHandle = true) : base(preexistingHandle, ownsHandle)
+        {
+        }
+
+        protected SafePowerRequestObject() : base()
+        {
+        }
+    }
+
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
     public class REASON_CONTEXT : IDisposable
     {
-        public DIAGNOSTIC_REASON_VERSION Version;
-        public DIAGNOSTIC_REASON Flags;
+        private readonly DIAGNOSTIC_REASON_VERSION _version;
+        private readonly DIAGNOSTIC_REASON _flags;
         private DETAIL _reason;
-
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-        public struct DETAIL
-        {
-            public IntPtr LocalizedReasonModule;
-            public uint LocalizedReasonId;
-            public uint ReasonStringCount;
-            public IntPtr ReasonStrings;
-        }
 
         public REASON_CONTEXT(string reason)
         {
-            Version = DIAGNOSTIC_REASON_VERSION.DIAGNOSTIC_REASON_VERSION;
-            Flags = DIAGNOSTIC_REASON.DIAGNOSTIC_REASON_SIMPLE_STRING;
-            _reason.LocalizedReasonModule = Marshal.StringToHGlobalUni(reason);
+            _version = DIAGNOSTIC_REASON_VERSION.DIAGNOSTIC_REASON_VERSION;
+            _flags = DIAGNOSTIC_REASON.DIAGNOSTIC_REASON_SIMPLE_STRING;
+            _reason._localizedReasonModule = Marshal.StringToHGlobalUni(reason);
         }
 
         void IDisposable.Dispose()
         {
-            if (Flags == DIAGNOSTIC_REASON.DIAGNOSTIC_REASON_SIMPLE_STRING)
-                Marshal.FreeHGlobal(_reason.LocalizedReasonModule);
+            if (_flags == DIAGNOSTIC_REASON.DIAGNOSTIC_REASON_SIMPLE_STRING && _reason._localizedReasonModule != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(_reason._localizedReasonModule);
+                // explicitly set to zero to prevent double free errors
+                _reason._localizedReasonModule = IntPtr.Zero;
+            }
+
+            GC.SuppressFinalize(this);
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct DETAIL
+        {
+            public IntPtr _localizedReasonModule;
+            public uint _localizedReasonId;
+            public uint _reasonStringCount;
+            public IntPtr _reasonStrings;
         }
     }
 
     public abstract class SafeKernelHandle : SafeHANDLE, IKernelHandle
     {
-        protected SafeKernelHandle() : base() { }
+        protected SafeKernelHandle() : base()
+        {
+        }
 
-        protected SafeKernelHandle(IntPtr preexistingHandle, bool ownsHandle = true) : base(preexistingHandle, ownsHandle) { }
+        protected SafeKernelHandle(IntPtr preexistingHandle, bool ownsHandle = true) : base(preexistingHandle, ownsHandle)
+        {
+        }
 
         protected override bool InternalReleaseHandle() => CloseHandle(handle);
     }
@@ -123,20 +132,23 @@ public interface IHandle
     IntPtr DangerousGetHandle();
 }
 
-public interface IKernelHandle : IHandle { }
-
-[StructLayout(LayoutKind.Sequential), DebuggerDisplay("{handle}")]
-public struct HANDLE : IHandle
+public interface IKernelHandle : IHandle
 {
-    private readonly IntPtr handle;
+}
 
-    public HANDLE(IntPtr preexistingHandle) => handle = preexistingHandle;
+[StructLayout(LayoutKind.Sequential)]
+[DebuggerDisplay("{handle}")]
+public readonly struct HANDLE : IHandle
+{
+    private readonly IntPtr _handle;
+
+    public HANDLE(IntPtr preexistingHandle) => _handle = preexistingHandle;
 
     public static HANDLE NULL => new(IntPtr.Zero);
 
-    public bool IsNull => handle == IntPtr.Zero;
+    public bool IsNull => _handle == IntPtr.Zero;
 
-    public static explicit operator IntPtr(HANDLE h) => h.handle;
+    public static explicit operator IntPtr(HANDLE h) => h._handle;
 
     public static implicit operator HANDLE(IntPtr h) => new(h);
 
@@ -148,11 +160,11 @@ public struct HANDLE : IHandle
 
     public static bool operator ==(HANDLE h1, HANDLE h2) => h1.Equals(h2);
 
-    public override bool Equals(object obj) => obj is HANDLE h && handle == h.handle;
+    public override bool Equals(object? obj) => obj is HANDLE h && _handle == h._handle;
 
-    public override int GetHashCode() => handle.GetHashCode();
+    public override int GetHashCode() => _handle.GetHashCode();
 
-    public IntPtr DangerousGetHandle() => handle;
+    public IntPtr DangerousGetHandle() => _handle;
 }
 
 [DebuggerDisplay("{handle}")]
@@ -166,6 +178,8 @@ public abstract class SafeHANDLE : SafeHandleZeroOrMinusOneIsInvalid, IEquatable
 
     public bool IsNull => handle == IntPtr.Zero;
 
+    public static implicit operator HANDLE(SafeHANDLE h) => h.handle;
+
     public static bool operator !(SafeHANDLE hMem) => hMem.IsInvalid;
 
     public static bool operator !=(SafeHANDLE h1, IHandle h2) => !(h1 == h2);
@@ -176,11 +190,10 @@ public abstract class SafeHANDLE : SafeHandleZeroOrMinusOneIsInvalid, IEquatable
 
     public static bool operator ==(SafeHANDLE h1, IntPtr h2) => h1?.Equals(h2) ?? false;
 
-    public static implicit operator HANDLE(SafeHANDLE h) => h.handle;
+    public bool Equals(SafeHANDLE? other) =>
+        ReferenceEquals(this, other) || (other is not null && handle == other.handle && IsClosed == other.IsClosed);
 
-    public bool Equals(SafeHANDLE other) => ReferenceEquals(this, other) || other is not null && handle == other.handle && IsClosed == other.IsClosed;
-
-    public override bool Equals(object obj) => obj switch
+    public override bool Equals(object? obj) => obj switch
     {
         IHandle ih => handle.Equals(ih.DangerousGetHandle()),
         SafeHandle sh => handle.Equals(sh.DangerousGetHandle()),
@@ -202,11 +215,21 @@ public abstract class SafeHANDLE : SafeHandleZeroOrMinusOneIsInvalid, IEquatable
     [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
     protected override bool ReleaseHandle()
     {
-        if (IsInvalid) return true;
-        if (!InternalReleaseHandle()) return false;
+        if (IsInvalid)
+        {
+            return true;
+        }
+
+        if (!InternalReleaseHandle())
+        {
+            return false;
+        }
+
         handle = IntPtr.Zero;
         return true;
     }
 
-    protected static T ThrowIfDisposed<T>(T h) where T : SafeHANDLE => h is null || h.IsInvalid ? throw new ObjectDisposedException(typeof(T).Name) : h;
+    protected static T ThrowIfDisposed<T>(T h)
+        where T : SafeHANDLE
+        => h is null || h.IsInvalid ? throw new ObjectDisposedException(typeof(T).Name) : h;
 }
