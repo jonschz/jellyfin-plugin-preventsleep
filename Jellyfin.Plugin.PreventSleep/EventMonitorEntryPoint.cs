a808659b85e22a0a7ca8b27776681ee9e6351e58
jonschz <jonschz@users.noreply.github.com>
2023-05-06 11:36:35 +0200

    Implement enabling/disabling timer, reduce complexity

--- Jellyfin.Plugin.PreventSleep/EventMonitorEntryPoint.cs
+++ Jellyfin.Plugin.PreventSleep/EventMonitorEntryPoint.cs
@@ -15,9 +15,7 @@ along with this program. If not, see<http://www.gnu.org/licenses/>.
 */
 
 using System;
-using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using MediaBrowser.Controller.Library;
@@ -31,15 +29,14 @@ namespace Jellyfin.Plugin.PreventSleep;
 
 public class EventMonitorEntryPoint : IServerEntryPoint
 {
-    private const int TimerInterval = 10000;
+    private const int TimerInterval = 20000;
     private readonly ISessionManager _sessionManager;
     private readonly ILogger<EventMonitorEntryPoint> _logger;
     private readonly object _powerRequestLock;
-    private readonly HybridDictionary _removedDevices;
-    private readonly Timer _unblockTimer;
+    // _unblockTimer is not null iff sleep mode is currently blocked
+    private Timer? _unblockTimer;
     private SafePowerRequestObject? _powerRequest;
     private TimeSpan _unblockSleepDelay;
-    private bool _blockingSleep;
     private bool _disposed;
     private DateTime _lastCheckin;
 
@@ -50,9 +47,7 @@ public class EventMonitorEntryPoint : IServerEntryPoint
         _logger = loggerFactory.CreateLogger<EventMonitorEntryPoint>();
         _sessionManager = sessionManager;
         _powerRequestLock = new object();
-        _removedDevices = new HybridDictionary(5);
         _lastCheckin = DateTime.MinValue;
-        _unblockTimer = new Timer(UnblockSleepTimerCallback, null, TimerInterval, TimerInterval);
         try
         {
             using var reasonContext = new REASON_CONTEXT("Jellyfin is serving files/waiting for the configured amount of time for further requests (blocked by Plugin.PreventSleep)");
@@ -68,35 +63,13 @@ public class EventMonitorEntryPoint : IServerEntryPoint
     {
         ApplySettingsFromConfig();
         _sessionManager.PlaybackProgress += SessionManager_PlaybackProgress;
-        _sessionManager.PlaybackStart += SessionManager_PlaybackStart;
-        _sessionManager.PlaybackStopped += SessionManager_PlaybackStop;
         Plugin.Instance!.ConfigurationChanged += Plugin_ConfigurationChanged;
 
         return Task.CompletedTask;
     }
 
-    private void SessionManager_PlaybackStart(object? sender, PlaybackProgressEventArgs e)
-    {
-        var deviceId = e.DeviceId;
-        if ((e.MediaInfo is not null) &&
-            (e.Users.Count > 0) &&
-            (e.Item is null || !e.Item.IsThemeMedia) &&
-            _removedDevices.Contains(deviceId))
-        {
-            _removedDevices.Remove(deviceId);
-            _logger.LogDebug("Removed {DeviceId} from list of removed devices", deviceId);
-        }
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
     private void SessionManager_PlaybackProgress(object? sender, PlaybackProgressEventArgs e)
     {
-        if (_removedDevices.Contains(e.DeviceId))
-        {
-            _logger.LogDebug("PlaybackProgress erroneously called for stopped device {DeviceName}", e.DeviceName);
-            return;
-        }
-
         // This most likely does not require locks / synchronisation; even if some other thread changes _lastCheckin
         // in between the read and the write below, not much can happen
         if (e.Session.LastPlaybackCheckIn > _lastCheckin)
@@ -104,19 +77,10 @@ public class EventMonitorEntryPoint : IServerEntryPoint
             _lastCheckin = e.Session.LastPlaybackCheckIn;
         }
 
-        BlockSleep();
-    }
-
-    private void SessionManager_PlaybackStop(object? sender, PlaybackStopEventArgs e)
-    {
-        var deviceId = e.DeviceId;
-        if (_removedDevices.Contains(deviceId))
+        if (_unblockTimer is null)
         {
-            return;
+            BlockSleep();
         }
-
-        _removedDevices.Add(deviceId, null);
-        _logger.LogDebug("Added {DeviceId} to list of removed devices", deviceId);
     }
 
     private void Plugin_ConfigurationChanged(object? sender, BasePluginConfiguration e)
@@ -126,33 +90,29 @@ public class EventMonitorEntryPoint : IServerEntryPoint
 
     private void ApplySettingsFromConfig()
     {
-        _unblockSleepDelay = TimeSpan.FromMinutes(Math.Clamp(Plugin.Instance!.Configuration.UnblockSleepDelay, 1, int.MaxValue));
+        // We add an additional delay of 15 seconds because Session.LastPlaybackCheckIn can lag behind UtcNow
+        // by 10 seconds (and possibly more) even if the stream if healthy.
+        const double AddedDelay = .25;
+        _unblockSleepDelay = TimeSpan.FromMinutes(
+            AddedDelay + Math.Clamp(Plugin.Instance!.Configuration.UnblockSleepDelay, 1, int.MaxValue));
     }
 
     private void BlockSleep()
     {
-        if (_blockingSleep)
-        {
-            return;
-        }
-
         lock (_powerRequestLock)
         {
-            // check _blockingSleep again for thread safety
-            if (_powerRequest is null || _blockingSleep)
+            // check _unblockTimer again for thread safety; we only ever change _unblockTimer
+            // when _powerRequestLock has been acquired
+            if (_powerRequest is null || _unblockTimer is not null)
             {
                 return;
             }
 
             try
             {
-                // TODO if PowerSetRequest fails, this will be re-attempted multiple times a second.
-                // Is there any chance that PowerSetRequest will fail on the first attempt but succeed soon after?
-                // If not, I would move "_blockingSleep = True" outside of this try-except block. Then, only a (failing)
-                // PowerClearRequest will be called later.
                 _logger.LogDebug("Attempting to block sleep");
                 SafePowerSetRequest(_powerRequest, POWER_REQUEST_TYPE.PowerRequestSystemRequired);
-                _blockingSleep = true;
+                _unblockTimer = new Timer(UnblockSleepTimerCallback, null, TimerInterval, TimerInterval);
                 _logger.LogDebug("PowerSetRequest succeeded: sleep blocked");
             }
             catch (Win32Exception err)
@@ -170,23 +130,16 @@ public class EventMonitorEntryPoint : IServerEntryPoint
     private void UnblockSleepTimerCallback(object? _)
     #pragma warning restore SA1313
     {
-        _logger.LogDebug("Timer to unblock sleep");
-        if (!_blockingSleep)
-        {
-            return;
-        }
-
         var timeSinceCheckin = DateTime.UtcNow - _lastCheckin;
+        _logger.LogDebug("Time since last checkin: {TimeElapsed}", timeSinceCheckin);
         if (timeSinceCheckin < _unblockSleepDelay)
         {
-            _logger.LogDebug("Time since checkin: {TimeElapsed}", timeSinceCheckin);
             return;
         }
 
         lock (_powerRequestLock)
         {
-            // check _blockingSleep again for thread safety
-            if (_powerRequest is null || !_blockingSleep)
+            if (_powerRequest is null || _unblockTimer is null)
             {
                 return;
             }
@@ -203,7 +156,8 @@ public class EventMonitorEntryPoint : IServerEntryPoint
                 return;
             }
 
-            _blockingSleep = false; // ignore errors
+            _unblockTimer.Dispose();
+            _unblockTimer = null;
         }
     }
 
@@ -217,19 +171,15 @@ public class EventMonitorEntryPoint : IServerEntryPoint
         if (disposing)
         {
             _sessionManager.PlaybackProgress -= SessionManager_PlaybackProgress;
-            _sessionManager.PlaybackStart -= SessionManager_PlaybackStart;
-            _sessionManager.PlaybackStopped -= SessionManager_PlaybackStop;
             Plugin.Instance!.ConfigurationChanged -= Plugin_ConfigurationChanged;
 
-            _unblockTimer.Dispose();
+            _unblockTimer?.Dispose();
 
             lock (_powerRequestLock)
             {
                 _powerRequest?.Dispose();
                 _powerRequest = null;
             }
-
-            _removedDevices.Clear();
         }
 
         _disposed = true;
